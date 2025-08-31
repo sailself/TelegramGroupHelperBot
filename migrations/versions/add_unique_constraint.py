9771a2c605ac3d348d414d71957bdcc4c2390fac
Nan <gokeeper@gmail.com>
2025-08-31 15:34:53 -0400

    migrations: add initial schema for messages and chain unique-constraint migration

--- migrations/versions/add_unique_constraint.py
+++ migrations/versions/add_unique_constraint.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'add_unique_constraint'
-down_revision = None
+down_revision = '53244dba3a71'
 branch_labels = None
 depends_on = None
 
